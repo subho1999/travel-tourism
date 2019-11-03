fb268e5036d4a98bb1bf756a6dee15c61531f2a5
Advaitva <guptaadvaitva@gmail.com>
2019-11-03 14:08:03 +0530

    css added

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let ejs=require('ejs');
 let body=require('body-parser');
 let request=require('request');
 let mongoose=require('mongoose');
-let dburl='mongodb+srv://sarthaksharma:abc1234@cluster0-ze17k.mongodb.net/test?retryWrites=true&w=majority/travelDB';
+let dburl='mongodb://localhost/travelDB';
 mongoose.connect(dburl,{useNewUrlParser:true,useUnifiedTopology:true});
 let db=mongoose.connection;
 db.on('error',console.error.bind(console,'Connnection Error -'));
@@ -12,13 +12,27 @@ db.once('open',()=>{
     console.log('Database Connected');
 });
 
+
+
+app.use(express.static(__dirname + '/public'));
 app.use(body.urlencoded({extended:true}));
 app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
-    res.render('mainPage');
+    res.render('index');
 });
-
+app.get('/packages',(req,res)=>{
+    res.render('package');
+});
+app.get('/signup',(req,res)=>{
+    res.render('signup');
+});
+app.get('/login',(req,res)=>{
+    res.render('login');
+});
+app.get('/experience',(req,res)=>{
+    res.render('experience');
+});
 
 app.listen(3000,process.env.ID,()=>{
     console.log('Server Started');
